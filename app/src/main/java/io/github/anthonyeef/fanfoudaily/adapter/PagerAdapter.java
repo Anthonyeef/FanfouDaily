957fb651057f570190fb4fbd40112137b81118a3
Anthonyeef <anthonyeef@gmail.com>
2015-10-14 14:34:34 +0800

    add PagerAdapter

---
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/adapter/PagerAdapter.java
@@ -0,0 +1,39 @@
+package io.github.anthonyeef.fanfoudaily.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anthonyeef on 10/14/15.
+ */
+public class PagerAdapter extends FragmentPagerAdapter{
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitles = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragments.add(fragment);
+        mFragmentTitles.add(title)
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitles.get(position);
+    }
+}
