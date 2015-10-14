209666423d37dc2337c518a9d9f3c8154b3ce596
Anthonyeef <anthonyeef@gmail.com>
2015-10-14 12:32:47 +0800

    finish FanfouAdapter

--- app/src/main/java/io/github/anthonyeef/fanfoudaily/adapter/FanfouAdapter.java
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/adapter/FanfouAdapter.java
@@ -1,13 +1,22 @@
 package io.github.anthonyeef.fanfoudaily.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
+import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
+import io.github.anthonyeef.fanfoudaily.R;
 import io.github.anthonyeef.fanfoudaily.model.Fanfou;
 
 /**
@@ -15,24 +24,61 @@ import io.github.anthonyeef.fanfoudaily.model.Fanfou;
  */
 public class FanfouAdapter extends RecyclerView.Adapter<FanfouAdapter.FanfouViewHolder>{
 
-    private List<Fanfou> mFanfouList;
+    private ArrayList<Fanfou> mFanfouList;
     private Context mContext;
 
-    public FanfouAdapter(Context context, List<Fanfou> mFanfouList) {
+    public FanfouAdapter(Context context, ArrayList<Fanfou> mFanfouList) {
         this.mFanfouList = mFanfouList;
         this.mContext = context;
     }
 
+    @Override
+    public int getItemCount() {
+        return mFanfouList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(final FanfouViewHolder fanfouViewHolder, int location){
+        Fanfou current = mFanfouList.get(location);
+
+        Uri avatar_uri = Uri.parse(current.getAvatarUrl());
+        Context context = fanfouViewHolder.vAvatar.getContext();
+        Picasso.with(context).load(avatar_uri)
+                .into(fanfouViewHolder.vAvatar);
+
+        fanfouViewHolder.vScreenName.setText(current.getScreenName());
+        fanfouViewHolder.vTimeStamp.setText(current.getTimeStamp());
+        fanfouViewHolder.vStatus.setText(current.getStatus());
+
+        if (current.getImageUrl() == null) {
+            fanfouViewHolder.vImage.setVisibility(View.GONE);
+        } else {
+            Uri image_uri = Uri.parse(current.getImageUrl());
+            Context context1 = fanfouViewHolder.vImage.getContext();
+
+            Picasso.with(context1).load(image_uri)
+                    .into(fanfouViewHolder.vImage);
+        }
+    }
+
+    @Override
+    public FanfouViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fanfou_item, viewGroup, false);
+        return new FanfouViewHolder(itemView);
+    }
+
     public static class FanfouViewHolder extends RecyclerView.ViewHolder {
 
-        protected TextView vScreenName;
-        protected TextView vStatus;
-        protected TextView vTimeStamp;
-        protected TextView vFavourite;
+        @Bind(R.id.avatar) CircleImageView vAvatar;
+        @Bind(R.id.name) TextView vScreenName;
+        @Bind(R.id.timestamp) TextView vTimeStamp;
+        @Bind(R.id.status) TextView vStatus;
+        @Bind(R.id.feedImage) ImageView vImage;
 
-        protected ImageView vImage;
-        protected CircleImageView vAvatar;
-
+        public FanfouViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
 
     }
 }
---
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/fragment/FanfouListFragment.java
@@ -0,0 +1,35 @@
+package io.github.anthonyeef.fanfoudaily.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.anthonyeef.fanfoudaily.R;
+import io.github.anthonyeef.fanfoudaily.adapter.FanfouAdapter;
+
+/**
+ * Created by anthonyeef on 10/14/15.
+ */
+public class FanfouListFragment extends Fragment{
+
+    private FanfouAdapter mFanfouAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView rv = (RecyclerView) inflater.inflate(
+                R.layout.fragment_item_list, container, false);
+        setupRecyclerView(rv);
+        return rv;
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.setAdapter(mFanfouAdapter);
+    }
+}
