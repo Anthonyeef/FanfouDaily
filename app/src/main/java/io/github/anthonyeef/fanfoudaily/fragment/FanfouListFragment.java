b16d37a8843ea230be070f60e8b72c199da1591b
Anthonyeef <anthonyeef@gmail.com>
2015-10-15 17:42:50 +0800

    find the api ! thank you .rex and Netputer

--- app/src/main/java/io/github/anthonyeef/fanfoudaily/fragment/FanfouListFragment.java
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/fragment/FanfouListFragment.java
@@ -14,8 +14,13 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -41,12 +46,47 @@ public class FanfouListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        fetchData();
+
+//        new Thread(runnable).start();
+
         RecyclerView rv = (RecyclerView) inflater.inflate(
                 R.layout.fragment_item_list, container, false);
         setupRecyclerView(rv);
         return rv;
     }
+
+//    private void show() {
+//
+//    }
+
+//    Runnable runnable = new Runnable() {
+//        @Override
+//        public void run() {
+//            org.jsoup.Connection connection = Jsoup.connect(weeklyUrl);
+//            connection.header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:32.0) Gecko/    20100101 Firefox/32.0");
+//            Document doc = null;
+//
+//            try {
+//                doc = connection.get();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            Elements fanfouElement = doc.select("tbody tr");
+//            Element nameElement = fanfouElement.select("a").first();
+//            Element statusElement = fanfouElement.select("span").first();
+//
+//            String name = nameElement.attr("title");
+//        }
+//    };
+//
+//    Handler handler = new Handler() {
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//            show();
+//        }
+//    };
 
 
     private void setupRecyclerView(RecyclerView recyclerView) {
@@ -55,59 +95,30 @@ public class FanfouListFragment extends Fragment {
         recyclerView.setAdapter(mFanfouAdapter);
     }
 
-    public void fetchData() {
-        StringRequest request = new StringRequest(Request.Method.GET, weeklyUrl, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (response != null) {
-                    Log.v(TAG, response);
+    JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, weeklyUrl, null, new Response.Listener<JSONObject>() {
+        @Override
+        public void onResponse(JSONObject response) {
+            Log.d(TAG, response.toString());
+            try {
+                JSONArray content = response.getJSONArray("msgs");
+                for (int i = 0; i < content.length(); i++) {
+                    Fanfou fanfou = new Fanfou();
+
+                    JSONObject fan = (JSONObject) content.get(i);
+                    String name = fan.getString("realname");
+                    String avatar = fan.getString("avatar");
+                    String time = fan.getString("time");
+                    String status = fan.getString("msg");
+                    JSONObject imageObj = fan.getJSONObject("img");
+                    String img = imageObj.getString("preview");
+
+                    fanfou.setScreenName(name);
+                    fanfou.setImageUrl(avatar);
+                    fan
                 }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.e("Error:", error.getMessage());
-            }
-        });
-
-        AppController.getInstance().addToRequestQueue(request, TAG);
-    }
-
-    private void parseData(String source) {
-        try {
-            Document doc = Jsoup.parse(source);
-            Element content = doc.select("div.content").first();
-            Element body = content.select("tbody").first();
-            Elements fanfouElement = body.select("td.item");
-
-            for (int i = 0; i < fanfouElement.size(); i++) {
-                Fanfou fanfou = new Fanfou();
-
-                Element feed = fanfouElement.get(i);
-                Element avatar = feed.select("div.avatar a[href]").first();
-                Element nameElement = feed.select("div.avatar a[title]").first();
-                Element statusElement = feed.select("div.text-outer span.text").first();
-                Element imageElement = feed.select("div.text-outer span.photo a img").first();
-
-                fanfou.setScreenName(nameElement.text());
-                fanfou.setAvatarUrl(avatar.text());
-                fanfou.setStatus(statusElement.text());
-
-                String image;
-                if (imageElement.attr("src") != "") {
-                    image = imageElement.attr("src");
-                } else {
-                    image = null;
-                }
-
-                fanfou.setImageUrl(image);
-
-                mFanfous.add(fanfou);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-        mFanfouAdapter.notifyDataSetChanged();
-    }
+    });
 }
