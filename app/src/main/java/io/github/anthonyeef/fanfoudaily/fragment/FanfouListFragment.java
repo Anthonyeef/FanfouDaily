ff844e0059541fb715ab5e5d01fd36096b95bde3
Anthonyeef <anthonyeef@gmail.com>
2015-10-15 23:35:31 +0800

    getting crazy by the mFanfous

--- app/src/main/java/io/github/anthonyeef/fanfoudaily/fragment/FanfouListFragment.java
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/fragment/FanfouListFragment.java
@@ -14,17 +14,11 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 
@@ -38,55 +32,22 @@ import io.github.anthonyeef.fanfoudaily.model.Fanfou;
  */
 public class FanfouListFragment extends Fragment {
     private static final String TAG = FanfouListFragment.class.getSimpleName();
-    private static final String weeklyUrl = "https://www.v2ex.com";
+    private static final String weeklyUrl = "http://blog.fanfou.com/digest/json/2015-10-14.daily.json";
 
     private FanfouAdapter mFanfouAdapter;
-    private ArrayList<Fanfou> mFanfous = null;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-//        new Thread(runnable).start();
+        fetchData(weeklyUrl);
 
         RecyclerView rv = (RecyclerView) inflater.inflate(
                 R.layout.fragment_item_list, container, false);
         setupRecyclerView(rv);
         return rv;
     }
-
-//    private void show() {
-//
-//    }
-
-//    Runnable runnable = new Runnable() {
-//        @Override
-//        public void run() {
-//            org.jsoup.Connection connection = Jsoup.connect(weeklyUrl);
-//            connection.header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:32.0) Gecko/    20100101 Firefox/32.0");
-//            Document doc = null;
-//
-//            try {
-//                doc = connection.get();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//            Elements fanfouElement = doc.select("tbody tr");
-//            Element nameElement = fanfouElement.select("a").first();
-//            Element statusElement = fanfouElement.select("span").first();
-//
-//            String name = nameElement.attr("title");
-//        }
-//    };
-//
-//    Handler handler = new Handler() {
-//        @Override
-//        public void handleMessage(Message msg) {
-//            super.handleMessage(msg);
-//            show();
-//        }
-//    };
 
 
     private void setupRecyclerView(RecyclerView recyclerView) {
@@ -95,30 +56,46 @@ public class FanfouListFragment extends Fragment {
         recyclerView.setAdapter(mFanfouAdapter);
     }
 
-    JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, weeklyUrl, null, new Response.Listener<JSONObject>() {
-        @Override
-        public void onResponse(JSONObject response) {
-            Log.d(TAG, response.toString());
-            try {
-                JSONArray content = response.getJSONArray("msgs");
-                for (int i = 0; i < content.length(); i++) {
-                    Fanfou fanfou = new Fanfou();
 
-                    JSONObject fan = (JSONObject) content.get(i);
-                    String name = fan.getString("realname");
-                    String avatar = fan.getString("avatar");
-                    String time = fan.getString("time");
-                    String status = fan.getString("msg");
-                    JSONObject imageObj = fan.getJSONObject("img");
-                    String img = imageObj.getString("preview");
+    private void fetchData(String url) {
+         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, (String) null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+                try {
+                    JSONArray message = response.getJSONArray("msgs");
+                    ArrayList<Fanfou> mFanfous = new ArrayList<Fanfou>(message.length());
+                    for (int i = 0; i < message.length(); i++) {
+                        Fanfou fan = new Fanfou();
+                        JSONObject fanfou = (JSONObject)message.get(i);
 
-                    fanfou.setScreenName(name);
-                    fanfou.setImageUrl(avatar);
-                    fan
+                        String name = fanfou.getString("realname");
+                        String avatar = fanfou.getString("avatar");
+                        String msg = fanfou.getString("msg");
+
+                        JSONObject image = fanfou.getJSONObject("img");
+                        String img = "";
+                        img = image.getString("preview");
+
+                        fan.setScreenName(name);
+                        fan.setAvatarUrl(avatar);
+                        fan.setStatus(msg);
+                        fan.setImageUrl(img);
+
+                        mFanfous.add(fan);
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
-        }
-    });
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error :" + error.getMessage());
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(request);
+    }
 }
