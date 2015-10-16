1a73ae7af869dd1f7d8b811fe1f9e626ae01b2e4
Anthonyeef <anthonyeef@gmail.com>
2015-10-16 22:40:20 +0800

    add urlendpoint file

---
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/extras/UrlEndpoints.java
@@ -0,0 +1,8 @@
+package io.github.anthonyeef.fanfoudaily.extras;
+
+/**
+ * Created by anthonyeef on 10/16/15.
+ */
+public class UrlEndpoints {
+    public static final String URL_BASE = "http://http://blog.fanfou.com/digest";
+}
