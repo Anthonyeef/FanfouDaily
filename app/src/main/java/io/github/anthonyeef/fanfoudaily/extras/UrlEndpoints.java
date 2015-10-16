fe2cb47c4389e971f2b3bcde44d8f966f0fd07a9
Anthonyeef <anthonyeef@gmail.com>
2015-10-16 22:44:00 +0800

    finish the endpoint list

--- app/src/main/java/io/github/anthonyeef/fanfoudaily/extras/UrlEndpoints.java
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/extras/UrlEndpoints.java
@@ -4,5 +4,9 @@ package io.github.anthonyeef.fanfoudaily.extras;
  * Created by anthonyeef on 10/16/15.
  */
 public class UrlEndpoints {
-    public static final String URL_BASE = "http://http://blog.fanfou.com/digest";
+    public static final String URL_BASE = "http://http://blog.fanfou.com/digest/";
+    public static final String URL_JSON = "json/";
+    public static final String URL_DAILY = ".daily";
+    public static final String URL_WEEKLY = ".weekly";
+    public static final String URL_END = ".json";
 }
