7f1244af196582244f03b3127821c7a6dddd631e
Anthonyeef <anthonyeef@gmail.com>
2015-10-16 23:01:38 +0800

    finish requestor for Volley queque

---
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/json/Requestor.java
@@ -0,0 +1,40 @@
+package io.github.anthonyeef.fanfoudaily.json;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by anthonyeef on 10/16/15.
+ */
+
+public class Requestor {
+    public static JSONObject requestJSON(RequestQueue requestQueue, String url) {
+        JSONObject response = null;
+        RequestFuture<JSONObject> requestFuture = RequestFuture.newFuture();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
+                url,
+                (String)null,
+                requestFuture, requestFuture);
+
+        requestQueue.add(request);
+        try {
+            response = requestFuture.get(30000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+
+        } catch (ExecutionException e) {
+
+        } catch (TimeoutException e) {
+
+        }
+        return response;
+    }
+}
