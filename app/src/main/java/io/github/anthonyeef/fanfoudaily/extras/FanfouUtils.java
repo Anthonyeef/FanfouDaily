dc51daec1f3da919a9117ff4a3e3e02712f263e0
Anthonyeef <anthonyeef@gmail.com>
2015-10-17 15:16:09 +0800

    finish fanfouUtils

--- app/src/main/java/io/github/anthonyeef/fanfoudaily/extras/FanfouUtils.java
+++ app/src/main/java/io/github/anthonyeef/fanfoudaily/extras/FanfouUtils.java
@@ -6,6 +6,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import io.github.anthonyeef.fanfoudaily.Utils.UrlUtils;
+import io.github.anthonyeef.fanfoudaily.json.Parser;
 import io.github.anthonyeef.fanfoudaily.json.Requestor;
 import io.github.anthonyeef.fanfoudaily.model.Fanfou;
 
@@ -13,7 +15,17 @@ import io.github.anthonyeef.fanfoudaily.model.Fanfou;
  * Created by anthonyeef on 10/16/15.
  */
 public class FanfouUtils {
-    public static ArrayList<Fanfou> loadFanfouFeeds(RequestQueue requestQueue) {
-        JSONObject response = Requestor.requestJSON(requestQueue, UrlEndpoints.)
+    public static ArrayList<Fanfou> loadFanfouDailyFeeds(RequestQueue requestQueue) {
+        JSONObject response = Requestor.requestJSON(requestQueue, UrlUtils.getRequestUrlFanfouDaily());
+        ArrayList<Fanfou> fanfous = Parser.parseFanfouJSON(response);
+
+        return fanfous;
+    }
+
+    public static ArrayList<Fanfou> loadFanfouWeeklyFeeds(RequestQueue requestQueue) {
+        JSONObject response = Requestor.requestJSON(requestQueue, UrlUtils.getRequestUrlFanfouWeekly());
+        ArrayList<Fanfou> fanfous = Parser.parseFanfouJSON(response);
+
+        return fanfous;
     }
 }
